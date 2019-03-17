1da6b1a98688a1252a1f46a96869dccedb08b0e3
marszhanglin <780965203@qq.com>
2019-03-17 15:42:16 +0800

    1.1.06
    springboot-1.1.06-Property

--- spb/src/main/java/com/mars/spb/controller/HelloWorldController.java
+++ spb/src/main/java/com/mars/spb/controller/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.mars.spb.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Value("${com.mars.spb.version}")
+    private String spbVersion;
+
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World2";
+        return "Hello World2,Version:"+spbVersion;
     }
 }
