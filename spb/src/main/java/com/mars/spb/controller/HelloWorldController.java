3ef1b579fe5661da4fce5b7dcb52214f5971ce7d
marszhanglin <780965203@qq.com>
2019-03-17 16:46:45 +0800

    1.1.07
    springboot-1.1.07-Logger

--- spb/src/main/java/com/mars/spb/controller/HelloWorldController.java
+++ spb/src/main/java/com/mars/spb/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.mars.spb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
     @Value("${com.mars.spb.version}")
     private String spbVersion;
 
     @RequestMapping("/hello")
     public String index() {
+        //SpringBoot默认给我们使用的是info级别的
+        //由低到高  trace<debug<info<warn<error
+        logger.trace("com.mars.spb.controller:trace");
+        logger.debug("com.mars.spb.controller:debug");
+        logger.info("com.mars.spb.controller:info");
+        logger.warn("com.mars.spb.controller:warn");
+        logger.error("com.mars.spb.controller:error");
         return "Hello World2,Version:"+spbVersion;
     }
 }
