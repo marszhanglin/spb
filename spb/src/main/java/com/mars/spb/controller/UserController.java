abf3b870c967aab77fac669852ad5272212cc68d
marszhanglin <780965203@qq.com>
2019-03-23 17:50:59 +0800

    1.1.08
    springboot-1.1.08-redis

---
+++ spb/src/main/java/com/mars/spb/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.mars.spb.controller;
+
+import com.mars.spb.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class UserController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @RequestMapping("/getUser")
+    public User getUser() {
+        ValueOperations<String, User> operations=redisTemplate.opsForValue();
+        User user=operations.get("user-key");
+        return user;
+    }
+
+
+    @RequestMapping("/setUser")
+    public User setUser() {
+        User user=new User("set2@126.com", "set", "set123456", "set","set123");
+        ValueOperations<String, User> operations=redisTemplate.opsForValue();
+        operations.set("user-key", user,2, TimeUnit.MINUTES);
+        //redisTemplate.delete("user-key");
+        return user;
+    }
+
+}
---
+++ spb/src/main/java/com/mars/spb/model/User.java
@@ -0,0 +1,73 @@
+package com.mars.spb.model;/**
+ * Created by Administrator on 2019/3/23.
+ */
+
+import java.io.Serializable;
+
+/**
+ * @author zhanglin
+ */
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public User(String email, String name, String password, String v1, String v2 ) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getV1() {
+        return v1;
+    }
+
+    public void setV1(String v1) {
+        this.v1 = v1;
+    }
+
+    public String getV2() {
+        return v2;
+    }
+
+    public void setV2(String v2) {
+        this.v2 = v2;
+    }
+
+    private String email;
+    private String name;
+    private String password;
+    private String v1;
+    private String v2;
+
+
+
+}
